777c1acb07310f5c25eaf7dc4535069b28759f9c
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2020-07-11 18:08:47 +0530

    Resources page

---
+++ src/data/resources.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Icon } from 'react-materialize';
+
+const Resources = [
+    {
+        field: 'A/V Editing',
+        icon: <Icon>camera_roll</Icon>,
+        url: 'https://ctrl.gq/avlr'
+    },
+    {
+        field: 'Web Development',
+        icon: <Icon>web</Icon>,
+        url: 'https://ctrl.gq/weblr'
+    },
+    {
+        field: 'Graphic Design',
+        icon: <Icon>color_lens</Icon>,
+        url: 'https://ctrl.gq/designlr'
+    },
+    {
+        field: '3D Design',
+        icon: <Icon>photo_library</Icon>,
+        url: 'https://ctrl.gq/3dlr'
+    },
+    {
+        field: 'Crossword',
+        icon: <Icon>text_snippet</Icon>,
+        url: 'https://ctrl.gq/crosslr'
+    },
+    {
+        field: 'Cryptic Hunts',
+        icon: <Icon>lock_open</Icon>,
+        url: 'https://ctrl.gq/encryptidlr'
+    }
+]
+
+export default Resources;
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
+import { Row, Col } from 'react-materialize';
+import Jumbotron from '../components/jumbotron';
+import data from '../data/resources';
 
 class ResourcesPage extends Component {
     render(){
         return (
+            <>
             <Helmet>
                 <title>TS | Resources</title>
             </Helmet>
+            <Jumbotron
+                title="Learning Resources"
+                desc="Tech Syndicate has members specializing in different fields, ranging from A/V Editing to Web Development. We have collated some learning resources for any newcomer who wishes to learn. These will give you brief overview of the different fields and help you navigate the most vital aspects of the same."
+            />
+            <Row>
+            {data.map(resource => (
+                <a href={resource.url} target="_blank" rel="noopener noreferrer">
+                    <Col l={4}>
+                    <div className="resource">
+                        <div className="resource-name">{resource.field}</div>
+                    </div>
+                    </Col>
+                </a>
+            ))}
+            </Row><br/>
+            </>
         )
     }
 }
