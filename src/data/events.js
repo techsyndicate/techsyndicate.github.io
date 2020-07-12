35c3a231f93159ab549be226af3fb354ba26c86a
Laxya Pahuja <31318248+laxyapahuja@users.noreply.github.com>
2020-07-12 14:58:01 +0530

    <br/>

--- src/data/events.js
+++ src/data/events.js
@@ -5,25 +5,25 @@ const Events = [
         title: 'Hackathon',
         eligibility: 'IX-XII',
         participants: '3',
-        description: <p>A classic online hackathon where plagiarism is scoffed at but StackOverflow is the way to go. <br>
+        description: <p>A classic online hackathon where plagiarism is scoffed at but StackOverflow is the way to go. <br/>
         Only six teams will be selected for the onsite event. Submit the URL of your Git repository and your website to hack@techsyndicate.co by 25th July.</p>,
         deliverables: 'Product and a website promoting your product',
-        prompt: 'https://ctrl.gq/hackprelims'
+        prompt: 'https://ctrl.gq/hack/prelims'
     },
     {
         title: 'Designathon',
         eligibility: 'IX-XII',
         participants: '3',
-        description: <p>“Design is not just what it looks like and feels like. Design is how it works.” - Steve Jobs <br>
+        description: <p>“Design is not just what it looks like and feels like. Design is how it works.” - Steve Jobs <br/>
         Only six teams will be selected for the onsite event. Submit the URL of an archive of your project to design@techsyndicate.co by 25th July.</p>,
         deliverables: 'Branding, Video (Teaser), 3D Model, Posters',
-        prompt: 'https://ctrl.gq/designprelims'
+        prompt: 'https://ctrl.gq/design/prelims'
     },
     {
         title: 'Crossword',
         eligibility: 'IX-XII',
         participants: '2',
-        description: <p>Prelims will take place on 25th July at https://ctrl.gq/crossprelims. <br>
+        description: <p>Prelims will take place on 25th July at https://ctrl.gq/crossprelims. <br/>
         Only six teams will be selected for the final round. </p>
     },
     {
@@ -32,7 +32,7 @@ const Events = [
         participants: '1',
         description: <p>Submit your entries at gd@techsyndicate.co by 25th July.</p>,
         deliverables: 'A video of you discussing your views on the topic.',
-        prompt: 'https://ctrl.gq/gdprelims'
+        prompt: 'https://ctrl.gq/gd/prelims'
     },
     {
         title: 'Surprise',
@@ -46,13 +46,13 @@ const Events = [
         participants: '1',
         description: <p>Submit your entries at audio@techsyndicate.co by 28th July.</p>,
         deliverables: A zip file containing the project file, samples used, and a README.,
-        prompt: 'https://ctrl.gq/audioprelims'
+        prompt: 'https://ctrl.gq/audio/prelims'
     },
     {
         title: 'Sr Robotics',
         eligibility: 'IX-XII',
         participants: '2',
-        description: <p>A virtual robotics competition testing your programming and logic building skills.<br>
+        description: <p>A virtual robotics competition testing your programming and logic building skills.<br/>
         The team which scores maximum points in least time will be the winner. Submit your entries at robotics@techsyndicate.co by 28th July.</p>,
         prompt: 'https://ctrl.gq/srrobotics'
     },
@@ -67,7 +67,7 @@ const Events = [
         title: 'Jr Quiz',
         eligibility: 'VII-VIII',
         participants: '2',
-        description: <p>Prelims will take place on 26th July at https://ctrl.gq/jrquizprelims. <br>
+        description: <p>Prelims will take place on 26th July at https://ctrl.gq/jrquiz/prelims. <br/>
         Only six teams will be selected for the final round.</p>
     },
     {
@@ -75,9 +75,9 @@ const Events = [
         eligibility: 'V-VI',
         participants: '1',
         description: <p>Topic: Against the odds charging ahead: Tribute to Teachers
-        Students need to create a webpage using the pictures, text and a small video on ADOBE SPARK. <br>
-        Submit the entries latest by 25th July 2020 on the email id : spark@techsyndicate.co.<br>
-        Judgement Criteria:<br>
+        Students need to create a webpage using the pictures, text and a small video on ADOBE SPARK. <br/>
+        Submit the entries latest by 25th July 2020 on the email id : spark@techsyndicate.co.<br/>
+        Judgement Criteria:<br/>
         <ul>
         <li>Impact</li>
         <li>Technical Skills</li>
@@ -88,10 +88,10 @@ const Events = [
         title: 'Paint 3D',
         eligibility: 'III-IV',
         participants: '1',
-        description: <p>Topic: For people who are risking their lives so another one can live theirs - A Tribute to the Corona Warriors <br>
-        Fire up the grandest Paint software program and do your best to create actual art. <br>
-        Submit your entries at paint3d@techsyndicate.co by 25th July.<br>
-        Judgement Criteria:<br>
+        description: <p>Topic: For people who are risking their lives so another one can live theirs - A Tribute to the Corona Warriors <br/>
+        Fire up the grandest Paint software program and do your best to create actual art. <br/>
+        Submit your entries at paint3d@techsyndicate.co by 25th July.<br/>
+        Judgement Criteria:<br/>
         <ul>
         <li>Originality</li>
         <li>Quality</li>
