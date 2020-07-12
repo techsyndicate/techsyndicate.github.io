f4e348a50d83186d79935f1cbeb7eada52d6cb4c
Laxya Pahuja <31318248+laxyapahuja@users.noreply.github.com>
2020-07-12 14:48:23 +0530

    events desc update

--- src/data/events.js
+++ src/data/events.js
@@ -5,67 +5,98 @@ const Events = [
         title: 'Hackathon',
         eligibility: 'IX-XII',
         participants: '3',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        description: <p>A classic online hackathon where plagiarism is scoffed at but StackOverflow is the way to go. <br>
+        Only six teams will be selected for the onsite event. Submit the URL of your Git repository and your website to hack@techsyndicate.co by 25th July.</p>,
+        deliverables: 'Product and a website promoting your product',
+        prompt: 'https://ctrl.gq/hackprelims'
     },
     {
         title: 'Designathon',
         eligibility: 'IX-XII',
         participants: '3',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        description: <p>“Design is not just what it looks like and feels like. Design is how it works.” - Steve Jobs <br>
+        Only six teams will be selected for the onsite event. Submit the URL of an archive of your project to design@techsyndicate.co by 25th July.</p>,
+        deliverables: 'Branding, Video (Teaser), 3D Model, Posters',
+        prompt: 'https://ctrl.gq/designprelims'
     },
     {
         title: 'Crossword',
         eligibility: 'IX-XII',
         participants: '2',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        description: <p>Prelims will take place on 25th July at https://ctrl.gq/crossprelims. <br>
+        Only six teams will be selected for the final round. </p>
     },
     {
         title: 'Group Discussion',
         eligibility: 'IX-XII',
         participants: '1',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        description: <p>Submit your entries at gd@techsyndicate.co by 25th July.</p>,
+        deliverables: 'A video of you discussing your views on the topic.',
+        prompt: 'https://ctrl.gq/gdprelims'
     },
     {
         title: 'Surprise',
         eligibility: 'IX-XII',
         participants: '2',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        description: <p>For you, the challenge has already begun. Make your way to the sever and await further instructions.</p>
     },
     {
         title: 'Audio Mixing',
         eligibility: 'IX-XII',
         participants: '1',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        description: <p>Submit your entries at audio@techsyndicate.co by 28th July.</p>,
+        deliverables: A zip file containing the project file, samples used, and a README.,
+        prompt: 'https://ctrl.gq/audioprelims'
     },
     {
         title: 'Sr Robotics',
         eligibility: 'IX-XII',
         participants: '2',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        description: <p>A virtual robotics competition testing your programming and logic building skills.<br>
+        The team which scores maximum points in least time will be the winner. Submit your entries at robotics@techsyndicate.co by 28th July.</p>,
+        prompt: 'https://ctrl.gq/srrobotics'
     },
     {
         title: 'Jr Robotics',
-        eligibility: 'VII-VIII',
+        eligibility: 'V-VIII',
         participants: '2',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        description: <p>Submit your entries at https://ctrl.gq/jrrobosub by 28th July.</p>,
+        prompt: 'https://ctrl.gq/jrrobotics'
     },
     {
         title: 'Jr Quiz',
         eligibility: 'VII-VIII',
         participants: '2',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    },
-    {
-        title: 'Paint 3D',
-        eligibility: 'VI-VIII',
-        participants: '1',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        description: <p>Prelims will take place on 26th July at https://ctrl.gq/jrquizprelims. <br>
+        Only six teams will be selected for the final round.</p>
     },
     {
         title: 'Adobe Spark',
-        eligibility: 'VI-VIII',
-        participants: '2',
-        description: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        eligibility: 'V-VI',
+        participants: '1',
+        description: <p>Topic: Against the odds charging ahead: Tribute to Teachers
+        Students need to create a webpage using the pictures, text and a small video on ADOBE SPARK. <br>
+        Submit the entries latest by 25th July 2020 on the email id : spark@techsyndicate.co.<br>
+        Judgement Criteria:<br>
+        <ul>
+        <li>Impact</li>
+        <li>Technical Skills</li>
+        <li>Creativity</li>
+        </ul></p>
+    },
+    {
+        title: 'Paint 3D',
+        eligibility: 'III-IV',
+        participants: '1',
+        description: <p>Topic: For people who are risking their lives so another one can live theirs - A Tribute to the Corona Warriors <br>
+        Fire up the grandest Paint software program and do your best to create actual art. <br>
+        Submit your entries at paint3d@techsyndicate.co by 25th July.<br>
+        Judgement Criteria:<br>
+        <ul>
+        <li>Originality</li>
+        <li>Quality</li>
+        <li>Creativity</li>
+        </ul></p>
     }
 ];
 
