0a335485c35aea026494dd0a8756ee37019c2207
Laxya Pahuja <31318248+laxyapahuja@users.noreply.github.com>
2020-07-27 22:54:47 +0530

    Update events.js

--- src/data/events.js
+++ src/data/events.js
@@ -6,14 +6,14 @@ const Events = [
         eligibility: 'IX-XII',
         participants: '3',
         description: <p>A classic online hackathon where plagiarism is scoffed at but StackOverflow is the way to go. <br/>
-        Only six teams will be selected for the onsite event. Submit the URL of your Git repository and your website to <a href="mailto:hack@techsyndicate.co">hack@techsyndicate.co</a> by 25th July.<br/>Deliverables: Source code of the app/web app and a pitch deck explaining your product.<br/><br/><a href="https://ctrl.gq/hack/prelims" target="_blank" rel="noopener noreferrer"><div className="prompt-button">Prelims</div></a></p>,
+        Only six teams will be selected for the onsite event. Submit the URL of your Git repository and your website to <a href="mailto:hack@techsyndicate.co">hack@techsyndicate.co</a> by 28th July.<br/>Deliverables: Source code of the app/web app and a pitch deck explaining your product.<br/><br/><a href="https://ctrl.gq/hack/prelims" target="_blank" rel="noopener noreferrer"><div className="prompt-button">Prelims</div></a></p>,
     },
     {
         title: 'Designathon',
         eligibility: 'IX-XII',
         participants: '3',
         description: <p>“Design is not just what it looks like and feels like. Design is how it works.” - Steve Jobs <br/>
-        Only six teams will be selected for the onsite event. Submit the URL of an archive of your project to <a href="mailto:design@techsyndicate.co">design@techsyndicate.co</a> by 25th July.<br/>Deliverables: Branding, Video (Teaser), 3D Model, Posters<br/><br/><br/><a href="https://ctrl.gq/design/prelims" target="_blank" rel="noopener noreferrer"><div className="prompt-button">Prelims</div></a></p>,
+        Only six teams will be selected for the onsite event. Submit the URL of an archive of your project to <a href="mailto:design@techsyndicate.co">design@techsyndicate.co</a> by 28th July.<br/>Deliverables: Branding, Video (Teaser), 3D Model, Posters<br/><br/><br/><a href="https://ctrl.gq/design/prelims" target="_blank" rel="noopener noreferrer"><div className="prompt-button">Prelims</div></a></p>,
     },
     {
         title: 'Encryptid',
@@ -32,7 +32,7 @@ const Events = [
         title: 'Group Discussion',
         eligibility: 'IX-XII',
         participants: '1',
-        description: <p>Submit your entries at <a href="mailto:gd@techsyndicate.co">gd@techsyndicate.co</a> by 25th July.<br/><br/>Deliverables: A video of you discussing your views on the topic.<br/><br/><br/><br/><br/><br/><br/><a href="https://ctrl.gq/gd/prelims" target="_blank" rel="noopener noreferrer"><div className="prompt-button">Prelims</div></a></p>,
+        description: <p>Submit your entries at <a href="mailto:gd@techsyndicate.co">gd@techsyndicate.co</a> by 28th July.<br/><br/>Deliverables: A video of you discussing your views on the topic.<br/><br/><br/><br/><br/><br/><br/><a href="https://ctrl.gq/gd/prelims" target="_blank" rel="noopener noreferrer"><div className="prompt-button">Prelims</div></a></p>,
     },
     {
         title: 'Surprise',
@@ -50,7 +50,7 @@ const Events = [
         title: 'Sr Robotics',
         eligibility: 'IX-XII',
         participants: '2',
-        description: <p>A virtual robotics competition testing your programming and logic building skills. The team which scores maximum points in least time will be the winner.<br/><br/>Submit your entries at <a href="robotics@techsyndicate.co">robotics@techsyndicate.co</a> by 28th July.<br/><br/><br/><br/><a href="https://ctrl.gq/srrobotics" target="_blank" rel="noopener noreferrer"><div className="prompt-button">Format</div></a></p>,
+        description: <p>A virtual robotics competition testing your programming and logic building skills. The team which scores maximum points in least time will be the winner.<br/><br/>Submit your entries at <a href="robotics@techsyndicate.co">robotics@techsyndicate.co</a> by 3rd August.<br/><br/><br/><br/><a href="https://ctrl.gq/srrobotics" target="_blank" rel="noopener noreferrer"><div className="prompt-button">Format</div></a></p>,
     },
     {
         title: 'Jr Robotics',
@@ -72,7 +72,7 @@ const Events = [
         participants: '1',
         description: <p>Topic: Against the odds charging ahead: Tribute to Teachers
         Students need to create a webpage using the pictures, text and a small video on Adobe Spark.<br/><br/>
-        Submit your entries <a href="mailto:spark@techsyndicate.co">here</a> latest by 25th July 2020.</p>
+        Submit your entries <a href="mailto:spark@techsyndicate.co">here</a> latest by 28th July 2020.</p>
     },
     {
         title: 'Paint 3D',
@@ -80,7 +80,7 @@ const Events = [
         participants: '1',
         description: <p>Topic: A Tribute to the Corona Warriors <br/>
         Fire up the grandest Paint software program and do your best to create actual art. <br/><br/>
-        Submit your entries <a href="paint3d@techsyndicate.co">here</a> latest by 25th July.</p>
+        Submit your entries <a href="paint3d@techsyndicate.co">here</a> latest by 28th July.</p>
     }
 ];
 
